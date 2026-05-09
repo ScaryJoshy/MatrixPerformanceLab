72c9f4fb7245df95e55d008f70be41ea93d08b79
Joshua Rios <riosjoshua15@gmail.com>
2026-05-08 21:34:06 -0500

    Test Matrix class in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,15 @@
 #include "Vector.h"
 #include <iostream>
+#include "../include/Matrix.h"
 
 int main()
-{
-    // Create two vectors
-    Vector v1({ 1, 2, 3 });
-    Vector v2({ 4, 5, 6 });
+{ 
+     std::cout << "--- Matrix Performance Lab ---" << std::endl;
 
-    // Show the original vectors
-    std::cout << "Vector 1: ";
-    v1.Print();
+    std::cout << "\n--- Matrix Test ---" << std::endl;
 
-    std::cout << "Vector 2: ";
-    v2.Print();
-
-    // Add them together
-    Vector sum = v1.Add(v2);
-
-    // Show the result
-    std::cout << "Sum: ";
-    sum.Print();
+    Matrix testMatrix(2, 3);
+    testMatrix.Print();
 
     return 0;
 }
